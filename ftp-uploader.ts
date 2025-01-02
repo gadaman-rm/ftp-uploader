2434d3a803f81b61b3746ece43224b7526951cee
gadaman <randm2001@gmail.com>
2025-01-02 11:40:17 +0330

    Test ok

---
+++ ftp-uploader.ts
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+
+import { Client } from "basic-ftp";
+import { Command } from "commander";
+import fs from "fs";
+import path from "path";
+
+const program = new Command();
+
+program
+  .requiredOption("-h, --host <string>", "FTP host")
+  .requiredOption("-u, --user <string>", "FTP username")
+  .requiredOption("-p, --password <string>", "FTP password")
+  .requiredOption("-f, --file <string>", "Local file path")
+  .requiredOption(
+    "-d, --directory <string>",
+    "Destination directory on FTP server"
+  )
+  .option("-n, --newname <string>", "New file name on the FTP server");
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+async function uploadFile() {
+  const client = new Client();
+
+  try {
+    console.log("Connecting to FTP server...");
+    await client.access({
+      host: options.host,
+      user: options.user,
+      password: options.password,
+    });
+
+    const localFilePath = path.resolve(options.file);
+    if (!fs.existsSync(localFilePath)) {
+      throw new Error(`File not found: ${localFilePath}`);
+    }
+
+    const newName = options.newname || path.basename(localFilePath);
+    const directory = options.directory.replace(/\\/g, "/");
+    const remotePath = path
+      .join(options.directory, newName)
+      .replace(/\\/g, "/");
+
+    console.log("Ensure remote directorry exist: ", directory);
+    await client.ensureDir(directory);
+    console.log(`Uploading ${options.file} to ${remotePath}...`);
+    await client.uploadFrom(options.file, newName);
+
+    console.log("Upload successful!");
+  } catch (err) {
+    console.error("Error during upload:", (err as Error).message);
+  } finally {
+    client.close();
+  }
+}
+
+uploadFile();
